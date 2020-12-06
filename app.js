666fa6ad29f366585221ad485ee5eade01c2044e
Amy Paschke <pasch049@umn.edu>
2020-12-06 12:12:27 -0600

    just me out here trying my best

--- app.js
+++ app.js
@@ -14,6 +14,66 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const questions = [
+  {
+    type: "input",
+    message: "What is the name of your employee?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the id of your employee?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the email of your employee?",
+    name: "email",
+  },
+];
+
+let managerQuestion = [
+    {
+        type: "input",
+        message: "What is the manager's office number?",
+        name: "officeNumber",
+      },
+]
+
+let internQuestion = [
+    {
+        type: "input",
+        message: "What is the school your intern attends?",
+        name: "school",
+      },
+]
+
+let engineerQuestion = [
+    {
+        type: "input",
+        message: "What is the engineer's GitHub?",
+        name: "github",
+      },
+]
+
+let typeQuestion = [
+    {
+        type: "list",
+        message: "Which type of employee would you like to add?",
+        name: "type",
+        choices: ["Intern", "Engineer", "Manager"],
+      },
+]
+
+let endQuestion = [
+    {
+        type: "list",
+        message: "Would you like to add another employee?",
+        name: "type",
+        choices: ["Yes", "No"],
+      },
+]
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -24,28 +84,40 @@ const render = require("./lib/htmlRenderer");
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
 let response;
 let collectingData = true;
 
-// let employees = [];
-//
-// while (collectingData) {
-//   response = inquirer.prompt("What type of Employee are you looking to add? [Manager, Engineer, Intern] (Leave blank to skip)")
+let employees = [];
+
+function employeeGenerator() {
+    return inquirer.prompt(typeQuestion).then(function(response){
+        if (response = "") {
+            collectingData = false;
+        }   else if (response == "Manager") {
+            inquirer.prompt(questions).prompt(managerQuestion).prompt(endQuestion).then(function(response){
+                if (response = "Yes") {
+                    employees.push(new Manager(name, id, email));
+                    //loop
+                    } else {
+                        employees.push(new Manager(name, id, email));
+                        inquirer.then(fsFunction());
+                    }
+                })
+            }
+    }) 
+}
+
+function fsFunction() {
+fs.writeFile(fileName, markdown, (error) =>
+    error ? console.log(error) : console.log())
+}
+
+while (collectingData) {
+   response = inquirer.prompt("What type of Employee are you looking to add? [Manager, Engineer, Intern] (Leave blank to skip)")
 //
 //   if (response = "") {
 //     collectingData = false;
-//   } else if (response == "Manager") {
-//     // Ask for manager details
+
 //     let name =
 //     let id =
 //     let email =
@@ -55,7 +127,7 @@ let collectingData = true;
 //   }
 // }
 
-let employees = [new Manager(), new Intern(), new Engineer()];
+//let employees = [new Manager(), new Intern(), new Engineer()];
 
 let html = render(employees);
 
